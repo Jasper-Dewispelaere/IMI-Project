4eefd2ddeca36aa043760cc9b1e18a03cb2415cf
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-02 12:26:11 +0200

    Add Director

---
+++ src/Imi.Project.Api.Core/Entities/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Director : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Film> Films { get; set; }
+    }
+}
