76d5957c4cf9d523cf9060b859ca3c743d7dd568
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-03 12:07:03 +0200

    Update ApplicationDbContext

---
+++ src/Imi.Project.Api.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; } 
+        public DbSet<Actor> Actors { get; set; } 
+        public DbSet<Director> Directors { get; set; } 
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
