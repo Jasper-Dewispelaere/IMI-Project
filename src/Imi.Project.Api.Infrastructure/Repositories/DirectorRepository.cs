62a701fc18fcbcbc360b9fb077bbad5571afad48
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-06 10:25:55 +0200

    Add DirectorRepository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/DirectorRepository.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Repositories.Interfaces;
+using Imi.Project.Api.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class DirectorRepository : BaseRepository<Director>, IDirectorRepository
+    {
+        public DirectorRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
