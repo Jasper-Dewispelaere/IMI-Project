26ba3d0da69c531f6818b6768f8e5465dd6213c1
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-06 10:27:13 +0200

    Add GenreRepository

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/GenreRepository.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Repositories.Interfaces;
+using Imi.Project.Api.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class GenreRepository : BaseRepository<Genre>, IGenreRepository
+    {
+        public GenreRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
