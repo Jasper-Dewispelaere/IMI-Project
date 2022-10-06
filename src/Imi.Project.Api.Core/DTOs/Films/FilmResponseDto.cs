d6a903febb00ffdbe7766ed8386847e6e1bfce57
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-06 10:48:45 +0200

    Add FilmResponseDto

---
+++ src/Imi.Project.Api.Core/DTOs/Films/FilmResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.DTOs.Films
+{
+    public class FilmResponseDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int ReleaseYear { get; set; }
+        public DirectorResponseDto Director { get; set; }
+        public GenreResponseDto Genre { get; set; } 
+        public ICollection<ActorResponseDto> Actor { get; set; } 
+    }
+}
