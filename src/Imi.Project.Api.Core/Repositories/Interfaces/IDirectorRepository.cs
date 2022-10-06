096737df52f2f18c512bc875a51f23e21fb3523e
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-06 10:11:29 +0200

    Add IDirectorRepository

---
+++ src/Imi.Project.Api.Core/Repositories/Interfaces/IDirectorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Repositories.Interfaces
+{
+    public interface IDirectorRepository : IBaseRepository<Director>
+    {
+    }
+}
