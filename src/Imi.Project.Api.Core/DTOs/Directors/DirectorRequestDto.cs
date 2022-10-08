e1257c1da52ce23d3243d5594b7b3eeb1371480d
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-08 13:01:59 +0200

    Add DirectorRequestDto

---
+++ src/Imi.Project.Api.Core/DTOs/Directors/DirectorRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.DTOs.Directors
+{
+    public class DirectorRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Name { get; set; }
+    }
+}
