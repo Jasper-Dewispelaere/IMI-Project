58fbcee5a33e95a69205ce7cd1e0f90f8c1d760f
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-08 12:14:58 +0200

    Add FilmRequestDto

---
+++ src/Imi.Project.Api.Core/DTOs/Films/FilmRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.DTOs.Films
+{
+    public class FilmRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public int ReleaseYear { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public Guid DirectorId { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public Guid GenreId { get; set; }
+    }
+}
