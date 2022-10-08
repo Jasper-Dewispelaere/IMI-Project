4bf9c87a34d7a137e524576443fd785c216b6a95
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-08 13:17:25 +0200

    Add GenreRequestDto

---
+++ src/Imi.Project.Api.Core/DTOs/Genres/GenreRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.DTOs.Genres
+{
+    public class GenreRequestDto : BaseDto
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public string Name { get; set; }
+    }
+}
