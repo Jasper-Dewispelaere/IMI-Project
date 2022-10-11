880b12e08507b04e31d08aa6cf68ee03ca7d4d70
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-11 13:25:41 +0200

    Update FilmsController

--- src/Imi.Project.Api/Controllers/FilmsController.cs
+++ src/Imi.Project.Api/Controllers/FilmsController.cs
@@ -4,6 +4,7 @@ using Imi.Project.Api.Core.DTOs.Actors;
 using Imi.Project.Api.Core.DTOs.Films;
 using Imi.Project.Api.Core.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Imi.Project.Api.Core.Entities;
 
 namespace Imi.Project.Api.Controllers
 {
@@ -51,6 +52,94 @@ namespace Imi.Project.Api.Controllers
             });
 
             return Ok(filmsDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var film = await _filmRepository.GetByIdAsync(id);
+            if (film == null)
+            {
+                return NotFound($"No film found with id: {id}.");
+            }
+            var filmDto = new FilmResponseDto()
+            {
+                Id = film.Id,
+                Title = film.Title,
+                ReleaseYear = film.ReleaseYear,
+                Image = film.Image,
+                Director = new DirectorResponseDto
+                { 
+                    Id = film.Director.Id,
+                    Name = film.Director.Name
+                },
+                Genre = new GenreResponseDto
+                {
+                    Id = film.Genre.Id,
+                    Name = film.Genre.Name
+                }
+            };
+            return Ok(filmDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(FilmRequestDto filmDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+            var film = new Film
+            {
+                Title = filmDto.Title,
+                ReleaseYear = filmDto.ReleaseYear,
+                Image = filmDto.Image,
+                DirectorId = filmDto.DirectorId,
+                GenreId = filmDto.GenreId
+            };
+            await _filmRepository.AddAsync(film);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(FilmRequestDto filmDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var film = await _filmRepository.GetByIdAsync(filmDto.Id);
+
+            if (film == null)
+            {
+                return NotFound($"Failed! No film found with id {filmDto.Id}");
+            }
+
+            film.Title = filmDto.Title;
+            film.ReleaseYear = filmDto.ReleaseYear;
+            film.Image = filmDto.Image;
+            film.DirectorId = filmDto.DirectorId;
+            film.GenreId = filmDto.GenreId;
+
+            await _filmRepository.UpdateAsync(film);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var film = await _filmRepository.GetByIdAsync(id);
+
+            if (film == null)
+            {
+                return NotFound($"Failed! No film found with id: {id}");
+            }
+
+            await _filmRepository.DeleteAsync(film);
+
+            return Ok();
         }
     }
 }
