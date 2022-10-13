6867d31733bc9a2c170802a06bf8c420c91edea5
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-13 09:16:54 +0200

    Add ApiBaseResponse

---
+++ src/Imi.Project.Wpf/ApiResponseModels/ApiBaseResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Wpf.ApiResponseModels
+{
+    public class ApiBaseResponse<T>
+    {
+        [JsonPropertyName("status")]
+        public bool Status { get; set; }
+
+        [JsonPropertyName("data")]
+        public T Data { get; set; }
+    }
+}
--- src/Imi.Project.Wpf/MainWindow.xaml.cs
+++ src/Imi.Project.Wpf/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +17,28 @@ using System.Windows.Shapes;
 
 namespace Imi.Project.Wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private HttpClient _httpClient;
+        public MainWindow(IHttpClientFactory httpClientFactory)
         {
             InitializeComponent();
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:5001/api/");
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e) 
+        {
+            var respone = await _httpClient.GetAsync("films");
+
+            if (respone.IsSuccessStatusCode)
+            {
+                using var responseStream = await respone.Content.ReadAsStreamAsync();
+                //var filmsResponse = await JsonSerializer.DeserializeAsync<ApiBaseResponse<IEnumerable<FilmsApiResponse>>>(responseStream);
+
+            }
         }
     }
 }
