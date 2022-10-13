aa829371839bbe6fb8d8b41bcdaa8ad83539f9db
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-13 10:53:45 +0200

    Update MainWindow

--- src/Imi.Project.Wpf/MainWindow.xaml.cs
+++ src/Imi.Project.Wpf/MainWindow.xaml.cs
@@ -59,6 +59,52 @@ namespace Imi.Project.Wpf
             }
         }
 
+        private void lstFilms_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var film = (FilmsApiResponse)lstFilms.SelectedItem;
+            if (film == null)
+            {
+                ClearFilmDetails();
+            }
+            else
+            {
+                GetFilmDetails(film);
+            }
+        }
+
+        private async void GetFilmDetails(FilmsApiResponse film)
+        {
+            var response = await _httpClient.GetAsync($"films/{film.Id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                var filmDetailResponse = await JsonSerializer.DeserializeAsync<FilmsApiResponse>(responseStream);
+
+                ShowFilmDetails(filmDetailResponse);
+            }
+            else
+            {
+                ShowFeedback(response.ReasonPhrase);
+            }
+        }
+
+        private void ShowFilmDetails(FilmsApiResponse filmDetail)
+        {
+            txtBlockTitle.Text = filmDetail.Title;
+            txtBlockReleaseYear.Text = filmDetail.ReleaseYear.ToString();
+            BitmapImage thumbnail = new BitmapImage();
+            thumbnail.BeginInit();
+            thumbnail.UriSource = new Uri(filmDetail.Image);
+            thumbnail.EndInit();
+            imgCover.Source = thumbnail;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void ShowFeedback(string message)
         {
             lblFeedback.Content = $"Fout: {message}";
@@ -67,9 +113,11 @@ namespace Imi.Project.Wpf
             lblFeedback.FontWeight = FontWeights.Bold;
         }
 
-        private void lstFilms_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ClearFilmDetails()
         {
-
+            txtBlockReleaseYear.Text = string.Empty;
+            txtBlockTitle.Text = string.Empty;
+            imgCover.Source = null;
         }
     }
 }
