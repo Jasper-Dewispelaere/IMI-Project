e8f15edfeabc1a6a60309127c3357d6fc4e27085
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-15 11:55:54 +0200

    Add GenresApiResponse

---
+++ src/Imi.Project.Wpf/ApiResponseModels/GenresApiResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Wpf.ApiResponseModels
+{
+    public class GenresApiResponse
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
