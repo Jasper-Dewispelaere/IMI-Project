57da5d95b16fe864f97093384d60f26987bf598a
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-17 10:53:31 +0200

    Add Actor

---
+++ src/Imi.Project.Blazor/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Blazor.Models
+{
+    public class Actor
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public ICollection<Film> Films { get; set; }
+    }
+}
