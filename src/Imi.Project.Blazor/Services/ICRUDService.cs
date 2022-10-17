38702cff06e81e67896c2597726eb4a242fd8890
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-17 11:00:33 +0200

    Add ICRUDService

---
+++ src/Imi.Project.Blazor/Services/ICRUDService.cs
@@ -0,0 +1,11 @@
+﻿namespace Imi.Project.Blazor.Services
+{
+    public interface ICRUDService<T> where T : class
+    {
+        Task<IQueryable<T>> GetAll(); 
+        Task<T> Get(int id); 
+        Task Create(T item); 
+        Task Update(T item); 
+        Task Delete(int id);
+    }
+}
