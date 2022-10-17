29ff45917950f17420121b6867b4a826bc1efda7
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-17 10:53:03 +0200

    Add Genre

---
+++ src/Imi.Project.Blazor/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imi.Project.Blazor.Models
+{
+    public class Genre
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+        public ICollection<Film> Films { get; set; }
+    }
+}
