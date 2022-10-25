1c3cec10da8d4fb8ca5c2b7acae8a08c0dd7a8cb
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-25 13:19:30 +0200

    Update QuizService

--- src/Imi.Project.Blazor/Services/QuizService.cs
+++ src/Imi.Project.Blazor/Services/QuizService.cs
@@ -1,9 +1,12 @@
-﻿using Imi.Project.Blazor.Models;
+﻿using Imi.Project.Blazor.Data.Seeding.QuizSeeding;
+using Imi.Project.Blazor.Models;
 
 namespace Imi.Project.Blazor.Services
 {
     public class QuizService : IQuizService<Quiz>
     {
+        List<Quiz> quizQuestions = QuestionSeeding.GetQuestions;
+
         public Task<Quiz[]> GetQuestion()
         {
             throw new NotImplementedException();
