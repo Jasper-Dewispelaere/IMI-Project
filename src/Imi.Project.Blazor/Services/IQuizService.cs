50989f7077e805c3ae402b2cc2cfe91c0f84c400
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-25 12:52:01 +0200

    Add QuizService

---
+++ src/Imi.Project.Blazor/Services/IQuizService.cs
@@ -0,0 +1,7 @@
+﻿namespace Imi.Project.Blazor.Services
+{
+    public interface IQuizService<T>
+    {
+        Task<T[]> GetQuestion();
+    }
+}
---
+++ src/Imi.Project.Blazor/Services/QuizService.cs
@@ -0,0 +1,12 @@
+﻿using Imi.Project.Blazor.Data;
+
+namespace Imi.Project.Blazor.Services
+{
+    public class QuizService : IQuizService<Quiz>
+    {
+        public Task<Quiz[]> GetQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
