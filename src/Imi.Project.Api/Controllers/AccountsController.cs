721adeda481b5658bcad0899d6671a9d6de4f930
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-27 10:09:54 +0200

    Add AccountsController

---
+++ src/Imi.Project.Api/Controllers/AccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+    }
+}
