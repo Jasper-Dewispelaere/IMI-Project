1ca849e2e764e5b2b151fc02ab3a3aaa911f5996
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-27 08:50:23 +0200

    Add ApplicationUser

---
+++ src/Imi.Project.Api.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime DateOfBirth { get; set; }
+        public bool AcceptTermsOfService { get; set; }
+    }
+}
