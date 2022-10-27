32a3adc5da829b021a3803b83d5c1f50c1f3ac87
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-27 09:33:09 +0200

    Update program

--- src/Imi.Project.Api/Program.cs
+++ src/Imi.Project.Api/Program.cs
@@ -1,13 +1,22 @@
+using Imi.Project.Api.Core.Entities;
 using Imi.Project.Api.Core.Repositories.Interfaces;
 using Imi.Project.Api.Infrastructure.Data;
 using Imi.Project.Api.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{ 
+    options.SignIn.RequireConfirmedEmail = false; 
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddCors();
 
@@ -21,6 +30,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(option => 
+{ 
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(jwtOptions => 
+    { 
+        jwtOptions.TokenValidationParameters = new TokenValidationParameters() 
+        { 
+            ValidateActor = true, 
+            ValidateAudience = true, 
+            ValidateLifetime = true, 
+            ValidIssuer = builder.Configuration["JWTConfiguration:Issuer"], 
+            ValidAudience = builder.Configuration["JWTConfiguration:Audience"], 
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTConfiguration:SigningKey"])) 
+        }; 
+    });
 
 var app = builder.Build();
 
@@ -37,6 +64,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
