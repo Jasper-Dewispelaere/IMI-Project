6e7f6f0480210bed0a1d0747d2d5df7a9a83ecb4
Jasper <Jasper.dewispelaere@student.howest.be>
2022-10-27 09:38:59 +0200

    Add RegisterUserRequestDto

---
+++ src/Imi.Project.Api.Core/DTOs/Accounts/RegisterUserRequestDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Core.DTOs.Accounts
+{
+    public class RegisterUserRequestDto
+    {
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)] 
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")] 
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public bool HasApprovedTermsAndConditions { get; set; }
+    }
+}
