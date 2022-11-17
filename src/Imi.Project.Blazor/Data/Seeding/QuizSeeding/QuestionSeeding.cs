5ca7f7ef19329dc40ee033d23385940223c12905
Jasper <Jasper.dewispelaere@student.howest.be>
2022-11-17 11:55:44 +0100

    Update QuestionSeeding

--- src/Imi.Project.Blazor/Data/Seeding/QuizSeeding/QuestionSeeding.cs
+++ src/Imi.Project.Blazor/Data/Seeding/QuizSeeding/QuestionSeeding.cs
@@ -42,6 +42,42 @@ namespace Imi.Project.Blazor.Data.Seeding.QuizSeeding
                         new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), PossibleAnswer = "Home Alone", IsRightAnswer = false }
                     }
             },
+            new QuizItem()
+            {
+                 Id = Guid.Parse("00000000-0000-0000-0000-000000000004"),
+                 Question = "Which film was directed by Andrew Adamson?",
+                 Answers = new List<Answer>
+                    {
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), PossibleAnswer = "Up", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), PossibleAnswer = "Back to the future", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), PossibleAnswer = "Cast Away", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), PossibleAnswer = "Shrek", IsRightAnswer = true }
+                    }
+            },
+            new QuizItem()
+            {
+                 Id = Guid.Parse("00000000-0000-0000-0000-000000000005"),
+                 Question = "In what year did Harry Potter and the Philosopher’s Stone release?",
+                 Answers = new List<Answer>
+                    {
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), PossibleAnswer = "2000", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), PossibleAnswer = "2001", IsRightAnswer = true },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), PossibleAnswer = "2002", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), PossibleAnswer = "2003", IsRightAnswer = false }
+                    }
+            },
+            new QuizItem()
+            {
+                 Id = Guid.Parse("00000000-0000-0000-0000-000000000006"),
+                 Question = "Who directed A Clockwork Orange?",
+                 Answers = new List<Answer>
+                    {
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000001"), PossibleAnswer = "Pete Docter", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000002"), PossibleAnswer = "Morten Tyldum", IsRightAnswer = false },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000003"), PossibleAnswer = "Stanley Kubrick", IsRightAnswer = true },
+                        new Answer() { Id = Guid.Parse("00000000-0000-0000-0000-000000000004"), PossibleAnswer = "James Cameron", IsRightAnswer = false }
+                    }
+            },
             //TODO: STILL NEED TO ADD MORE
         };
 
