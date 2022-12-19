fee211dccdcc1cb830e9e9d0b9748e68a36efe30
Jasper <Jasper.dewispelaere@student.howest.be>
2022-12-19 11:41:01 +0100

    Add FilmApiService

---
+++ src/Imi.Project.Blazor/Services/Api/FilmApiService.cs
@@ -0,0 +1,32 @@
+﻿using Imi.Project.Blazor.Models;
+
+namespace Imi.Project.Blazor.Services.Api
+{
+    public class FilmApiService : ICRUDService<Film>
+    {
+        public Task Create(Film item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Film> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Film>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Film item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
