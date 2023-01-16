69ca3d59cf50f1bf43719912493f1d940f3520ac
Jasper <Jasper.dewispelaere@student.howest.be>
2023-01-16 16:51:31 +0100

    Update Program

--- src/Imi.Project.Api/Program.cs
+++ src/Imi.Project.Api/Program.cs
@@ -55,7 +55,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
 { 
-    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Bull.com API", Version = "v1" });
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Imi API", Version = "v1" });
     option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
