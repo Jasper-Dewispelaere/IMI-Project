86fa5df9ffd34b4261b6194d77ec2b36880d50df
Jasper <Jasper.dewispelaere@student.howest.be>
2023-01-16 19:07:50 +0100

    Update Register

--- src/Imi.Project.Vue/Controllers/HomeController.cs
+++ src/Imi.Project.Vue/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace Imi.Project.Vue.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
