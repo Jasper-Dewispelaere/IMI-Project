028be11a82f714c3b8b1b0db29a9d3aae6037d8f
Jasper <Jasper.dewispelaere@student.howest.be>
2023-01-16 11:07:32 +0100

    Update FilmImdb

--- src/Imi.Project.Vue/Controllers/HomeController.cs
+++ src/Imi.Project.Vue/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Imi.Project.Vue.Controllers
         {
             return View();
         }
+        public IActionResult FilmImdb()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
